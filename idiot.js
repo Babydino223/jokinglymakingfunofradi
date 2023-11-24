c04bdffc710b9fb1aab84a1d36549e1c9da1f279
Babydino223 <84541716+Babydino223@users.noreply.github.com>
2023-11-24 00:54:55 -0500

    importing base code

---
+++ idiot.js
@@ -0,0 +1,17 @@
+function isIdiot(person) {
+  if (person.FuckingStupid == true) {
+    console.log("haha that person's a fucking idiot")
+  	} else {
+    console.log("not a fucking idiot")
+    }
+}
+const tyto = {
+   FuckingStupid: false
+};
+const radi = {
+  FuckingStupid: true
+}
+const alex = {
+  FuckingStupid: true
+}
+isIdiot(radi);
